d6b4403267fc89800116fdc88ee89fae0e317b55
asabry18 <mohamedmahrous581@gmail.com>
2024-10-29 19:15:53 +0300

    initialize project

---
+++ models/Menu.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const menuItemSchema = new Schema({
+	name: String,
+	price: Number,
+	category: String,
+    imageUrl: String,
+    rating: Number
+});
+
+const MenuItem = mongoose.model("MenuItem", menuItemSchema);
+
+module.exports = MenuItem;
