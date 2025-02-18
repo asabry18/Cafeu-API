8f44de5ec3eb6273315098711fce06f76ab0cbf1
Ahmed Sabry <mohamedmahrous581@gmail.com>
2025-02-18 13:39:51 +0200

    add post & get & delete endpoint

--- endpoints/reservation/insertReservation.js
+++ endpoints/reservation/insertReservation.js
@@ -4,14 +4,16 @@ const insertReservation= async (req, res) => {
     // initialize new reservation 
     const reservation = new reservations();
 
-    reservation.cardImageUrl = req.body.cardImageUrl
-    reservation.title = req.body.title
-    reservation.body =  req.body.body
-    reservation.date =  req.body.date
+    reservation.date = req.body.date
+    reservation.time = req.body.time
+    reservation.name =  req.body.name
+    reservation.phone =  req.body.phone
+    reservation.totalPerson =  req.body.totalPerson
 
     // insert to DB
     reservation.save();
     res.send('reservation has been saved')
+    
 }
 
 module.exports = {insertReservation}
